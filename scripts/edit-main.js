5317e4c738f6ac8c7fa92af94de5c5fdc261492b
John Lindquist <johnlindquist@gmail.com>
2021-03-23 14:58:28 -0600

    Delete edit-main.js

--- scripts/edit-main.js
+++
@@ -1,7 +0,0 @@
-//Menu: Edit Main Script
-//Description: Edit the script/shortcut that opens this menu
-
-//Note: Feel free to delete this script!
-//Type cmd + shift + ; , then type "remove"
-
-edit(kenvPath("app/left-click.js"), kenvPath())
