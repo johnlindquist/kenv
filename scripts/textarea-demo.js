585aeb37cc488672a168b6f3f8c4944dd6c5af5e
John Lindquist <johnlindquist@gmail.com>
2021-08-11 22:52:46 -0600

    cleaning up kenv template

--- scripts/textarea-demo.js
+++
@@ -1,8 +0,0 @@
-// Menu: Textarea Demo
-// Description: For longer text entry
-// Author: John Lindquist
-// Twitter: @johnlindquist
-
-let value = await textarea()
-await writeFile(tmp("textarea-demo.txt"), value)
-edit(tmp("textarea-demo.txt"))
